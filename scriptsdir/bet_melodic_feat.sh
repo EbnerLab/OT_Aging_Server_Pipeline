cb8c37d264cf8acacb9bc821de37ea5932f227cc
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2017-09-21 19:01:52 -0400

    Add files via upload

---
+++ scriptsdir/bet_melodic_feat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=camctrp
+#SBATCH --qos=camctrp
+#SBATCH --job-name=bet_melodic_feat
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=dgulliford@ufl.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=8gb
+#SBATCH --time=12:00:00
+#SBATCH --output=bet_melodic_feat_%j.out
+pwd; hostname; date
+
+module load fsl/5.0.8
+
+fslreorient2std T1/T1.nii.gz FSL/T1/T1_reorient.nii.gz
+bet FSL/T1/T1_reorient.nii.gz FSL/T1/T1_brain.nii.gz -f .1 -B -R
+feat rs_melodic.fsf
+feat hs_feat.fsf
+date
