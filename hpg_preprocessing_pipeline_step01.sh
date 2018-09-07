c43aefff2f18bc72c75b4f02fc8ee3c8bb430891
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2018-09-07 18:05:30 -0400

    Update hpg_preprocessing_pipeline_step01.sh

--- hpg_preprocessing_pipeline_step01.sh
+++ hpg_preprocessing_pipeline_step01.sh
@@ -133,8 +133,62 @@ do
 #drops down in to subject folder and starts freesurfer run
  	cd ${a}${visit}/
 		sbatch freesurfer_run.sh
-	cd ../
  	echo "eddy freesurfer run started for " ${a}${visit}
+	
+#drops down into hsBOLD directory and creates timing files for heider simmel
+    cd hsBOLD/
+
+    while IFS= read -r line; do
+        if [[ $line =~ StimType ]]; then
+            echo "$line" >>stimtype.txt
+        fi
+    done <eprime.txt
+
+    echo "16000" >>durationextract.txt
+
+#extracts stimulus and fixation durations in milliseconds from original eprime text file and writes to a new file "durationextract"
+    while IFS= read -r line; do
+        if [[ $line =~ VidBlock.Duration\: ]] || [[ $line =~ FixationBlock.Duration\: ]]; then
+            echo "$line" >>durationextract.txt
+        fi
+    done <eprime.txt
+
+#converts stimulus duration times from durationextract.txt from milliseconds to seconds, computes onsets based on previous stimuli durations, creates timing columns, and writes the information to a new file "allcolumn"
+    onset=0
+
+    while IFS= read -r line; do
+        if [[ $line =~ 16000 ]]; then
+	        echo "$onset	16	1" >>allcolumn.txt
+	        onset=$(($onset + 16))
+        elif [[ $line =~ 15000 ]]; then
+	        echo "$onset	15	1" >>allcolumn.txt
+	        onset=$(($onset + 15))
+        elif [[ $line =~ 17000 ]]; then
+	        echo "$onset	17	1" >>allcolumn.txt
+	        onset=$(($onset + 17))
+        fi
+    done <durationextract.txt
+
+#creates separate files for stimuli and fixation, assuming that the first and all odd presentations are fixation and even presentations are stimuli
+    awk 'NR % 2 == 0' allcolumn.txt >>stimtiming.txt
+    awk 'NR % 2 == 1' allcolumn.txt >>fixationtiming.txt
+
+#creates separate timing files for stimuli types, searching first in stimtype.txt to determine counterbalance and write timing files accordingly
+    line=$(head -1 stimtype.txt)
+        if [[ $line =~ Nonsocial ]]; then
+	        awk 'NR % 2 == 0' stimtiming.txt >>socialtiming.txt
+	        awk 'NR % 2 == 1' stimtiming.txt >>nonsocialtiming.txt
+        elif [[ $line =~ Social ]]; then
+	        awk 'NR % 2 == 1' stimtiming.txt >>socialtiming.txt
+	        awk 'NR % 2 == 0' stimtiming.txt >>nonsocialtiming.txt
+        fi
+
+#deletes unnecessary files
+    rm stimtype.txt durationextract.txt allcolumn.txt stimtiming.txt
+
+    echo "fixation and stimulus timing file creation complete"
+    
+    cd ../../
 
 echo ${a}${visit} "completed, moving on..."
 
