0b741b508271075c7bac4af9dda269533c9d49dc
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2018-09-07 18:13:36 -0400

    edited memory and removed bet

--- scriptsdir/melodic_feat.sh
+++ scriptsdir/melodic_feat.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=<email_associated_with_hipergator_account>
 #SBATCH --ntasks=1
-#SBATCH --mem=8gb
+#SBATCH --mem=4gb
 #SBATCH --time=12:00:00
 #SBATCH --output=bet_melodic_feat_%j.out
 pwd; hostname; date
